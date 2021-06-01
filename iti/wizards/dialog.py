bfe0fc7eeba3388004805d83a2628cdd6cb40aaa
youssefshaban <youssef.shaban.atta@gmail.com>
2021-06-01 11:05:32 -0700

    lab2

--- iti/wizards/dialog.py
+++ iti/wizards/dialog.py
@@ -11,6 +11,24 @@ class dialog(models.TransientModel):
     def createNewOrderWithSaveValues(self):
         self.ensure_one()
         order = self.env['orders'].browse(self._context.get('active_ids', False))
-      
-
-        print(order.name)
+        print (order.order_lines)
+        # new_instance = self.env['orders'].create([order])
+        customer = self.env['res.partner'].browse(order.name.id)
+        print (customer.id)
+        newOrder = self.env['orders'].create({
+            'date': order.date,
+            'name':customer.id,
+            'total':order.total
+            })
+        for  lines in order.order_lines :
+            medicine = self.env['medicine'].browse(lines.name.id)
+            order = self.env['orders'].browse(newOrder.id)
+            self.env['order_lines'].create(
+            {
+                'name':medicine.id,
+                'qty':lines.qty,
+                'sub_total':lines.sub_total,
+                'orderItem': order.id
+            }
+        )
+        # print(new_instance)
